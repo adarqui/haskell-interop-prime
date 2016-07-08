c701c8969b636a8755c8252870ceabd1b19d4765
Andrew Darqui <andrew.darqui@gmail.com>
2016-07-08 05:26:54 -0400

    generated

--- gen/Interop.Convert.Clean.hs
+++ gen/Interop.Convert.Clean.hs
@@ -11,8 +11,10 @@ module Interop.Convert.Clean where
 
 
 import           Data.Aeson          (FromJSON, ToJSON (), Value (..), parseJSON, toJSON, object, (.=), (.:))
+import           Data.Int            (Int64)
 import           Data.Text           (Text)
 import qualified Data.Text           as T
+import           Data.Time           (UTCTime)
 import           Data.Monoid         ((<>))
 import           Haskell.Api.Helpers (QueryParam, qp)
 
