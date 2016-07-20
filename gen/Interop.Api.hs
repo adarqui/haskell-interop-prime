e407e81662f446edcba4ec1ee76e820e3ede27a0
Andrew Darqui <andrew.darqui@gmail.com>
2016-07-20 01:19:16 -0400

    generated

--- gen/Interop.Api.hs
+++ gen/Interop.Api.hs
@@ -13,101 +13,102 @@ module Interop.Api where
 
 
 
-import Data.Int            (Int64)
-import Data.Monoid         ((<>))
-import Data.Text           (Text)
-import qualified Data.Text as T (pack)
-import Haskell.Api.Helpers (ApiEff, ApiError, QueryParam, qp, handleError, getAt, putAt, postAt, deleteAt)
-import Data.Default        (Default, def)
+import Data.Int                   (Int64)
+import Data.Monoid                ((<>))
+import Data.Text                  (Text)
+import qualified Data.Text        as T (pack)
+import Haskell.Api.Helpers.Shared (ApiEff, ApiError, QueryParam, qp)
+import Haskell.Api.Helpers        (SpecificApiOptions, handleError, getAt, putAt, postAt, deleteAt)
+import Data.Default               (Default, def)
 
-getUsers :: forall qp. QueryParam qp => [qp] -> ApiEff (Either (ApiError ApplicationError) UserResponses)
+getUsers :: forall qp. QueryParam qp => [qp] -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponses)
 getUsers params = handleError <$> getAt params ["users"]
 
-getUsers' :: ApiEff (Either (ApiError ApplicationError) UserResponses)
+getUsers' :: ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponses)
 getUsers'  = handleError <$> getAt ([] :: [(Text, Text)]) ["users"]
 
-getUsers_ByUsersIds :: forall qp. QueryParam qp => [qp] -> [Int64] -> ApiEff (Either (ApiError ApplicationError) UserResponses)
+getUsers_ByUsersIds :: forall qp. QueryParam qp => [qp] -> [Int64] -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponses)
 getUsers_ByUsersIds params _ByUsersIds = handleError <$> getAt (map qp params <> map qp [ByUsersIds _ByUsersIds]) ["users"]
 
-getUsers_ByUsersIds' :: [Int64] -> ApiEff (Either (ApiError ApplicationError) UserResponses)
+getUsers_ByUsersIds' :: [Int64] -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponses)
 getUsers_ByUsersIds' _ByUsersIds = handleError <$> getAt [ByUsersIds _ByUsersIds] ["users"]
 
-getUsers_ByUsersNames :: forall qp. QueryParam qp => [qp] -> [String] -> ApiEff (Either (ApiError ApplicationError) UserResponses)
+getUsers_ByUsersNames :: forall qp. QueryParam qp => [qp] -> [String] -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponses)
 getUsers_ByUsersNames params _ByUsersNames = handleError <$> getAt (map qp params <> map qp [ByUsersNames _ByUsersNames]) ["users"]
 
-getUsers_ByUsersNames' :: [String] -> ApiEff (Either (ApiError ApplicationError) UserResponses)
+getUsers_ByUsersNames' :: [String] -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponses)
 getUsers_ByUsersNames' _ByUsersNames = handleError <$> getAt [ByUsersNames _ByUsersNames] ["users"]
 
-getUsers_ByUsersEmails :: forall qp. QueryParam qp => [qp] -> [String] -> ApiEff (Either (ApiError ApplicationError) UserResponses)
+getUsers_ByUsersEmails :: forall qp. QueryParam qp => [qp] -> [String] -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponses)
 getUsers_ByUsersEmails params _ByUsersEmails = handleError <$> getAt (map qp params <> map qp [ByUsersEmails _ByUsersEmails]) ["users"]
 
-getUsers_ByUsersEmails' :: [String] -> ApiEff (Either (ApiError ApplicationError) UserResponses)
+getUsers_ByUsersEmails' :: [String] -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponses)
 getUsers_ByUsersEmails' _ByUsersEmails = handleError <$> getAt [ByUsersEmails _ByUsersEmails] ["users"]
 
-getUser_ByUserActive :: forall qp. QueryParam qp => [qp] -> Int64 -> Bool -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+getUser_ByUserActive :: forall qp. QueryParam qp => [qp] -> Int64 -> Bool -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 getUser_ByUserActive params user_id _ByUserActive = handleError <$> getAt (map qp params <> map qp [ByUserActive _ByUserActive]) ["user", T.pack $ show user_id]
 
-getUser_ByUserActive' :: Int64 -> Bool -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+getUser_ByUserActive' :: Int64 -> Bool -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 getUser_ByUserActive' user_id _ByUserActive = handleError <$> getAt [ByUserActive _ByUserActive] ["user", T.pack $ show user_id]
 
-postUser :: forall qp. QueryParam qp => [qp] -> UserRequest -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+postUser :: forall qp. QueryParam qp => [qp] -> UserRequest -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 postUser params user_request = handleError <$> postAt params ["user"] user_request
 
-postUser' :: UserRequest -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+postUser' :: UserRequest -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 postUser' user_request = handleError <$> postAt ([] :: [(Text, Text)]) ["user"] user_request
 
-getUser :: forall qp. QueryParam qp => [qp] -> Int64 -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+getUser :: forall qp. QueryParam qp => [qp] -> Int64 -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 getUser params user_id = handleError <$> getAt params ["user", T.pack $ show user_id]
 
-getUser' :: Int64 -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+getUser' :: Int64 -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 getUser' user_id = handleError <$> getAt ([] :: [(Text, Text)]) ["user", T.pack $ show user_id]
 
-putUser :: forall qp. QueryParam qp => [qp] -> Int64 -> UserRequest -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+putUser :: forall qp. QueryParam qp => [qp] -> Int64 -> UserRequest -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 putUser params user_id user_request = handleError <$> putAt params ["user", T.pack $ show user_id] user_request
 
-putUser' :: Int64 -> UserRequest -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+putUser' :: Int64 -> UserRequest -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 putUser' user_id user_request = handleError <$> putAt ([] :: [(Text, Text)]) ["user", T.pack $ show user_id] user_request
 
-deleteUser :: forall qp. QueryParam qp => [qp] -> Int64 -> ApiEff (Either (ApiError ApplicationError) ())
+deleteUser :: forall qp. QueryParam qp => [qp] -> Int64 -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) ())
 deleteUser params user_id = handleError <$> deleteAt params ["user", T.pack $ show user_id]
 
-deleteUser' :: Int64 -> ApiEff (Either (ApiError ApplicationError) ())
+deleteUser' :: Int64 -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) ())
 deleteUser' user_id = handleError <$> deleteAt ([] :: [(Text, Text)]) ["user", T.pack $ show user_id]
 
-getUserText :: forall qp. QueryParam qp => [qp] -> String -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+getUserText :: forall qp. QueryParam qp => [qp] -> String -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 getUserText params user_name = handleError <$> getAt params ["user_text", user_name]
 
-getUserText' :: String -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+getUserText' :: String -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 getUserText' user_name = handleError <$> getAt ([] :: [(Text, Text)]) ["user_text", user_name]
 
-putUserText :: forall qp. QueryParam qp => [qp] -> String -> UserRequest -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+putUserText :: forall qp. QueryParam qp => [qp] -> String -> UserRequest -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 putUserText params user_name user_request = handleError <$> putAt params ["user_text", user_name] user_request
 
-putUserText' :: String -> UserRequest -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+putUserText' :: String -> UserRequest -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 putUserText' user_name user_request = handleError <$> putAt ([] :: [(Text, Text)]) ["user_text", user_name] user_request
 
-deleteUserText :: forall qp. QueryParam qp => [qp] -> String -> ApiEff (Either (ApiError ApplicationError) ())
+deleteUserText :: forall qp. QueryParam qp => [qp] -> String -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) ())
 deleteUserText params user_name = handleError <$> deleteAt params ["user_text", user_name]
 
-deleteUserText' :: String -> ApiEff (Either (ApiError ApplicationError) ())
+deleteUserText' :: String -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) ())
 deleteUserText' user_name = handleError <$> deleteAt ([] :: [(Text, Text)]) ["user_text", user_name]
 
-getUserString :: forall qp. QueryParam qp => [qp] -> [Char] -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+getUserString :: forall qp. QueryParam qp => [qp] -> [Char] -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 getUserString params user_name = handleError <$> getAt params ["user_string", T.pack $ show user_name]
 
-getUserString' :: [Char] -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+getUserString' :: [Char] -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 getUserString' user_name = handleError <$> getAt ([] :: [(Text, Text)]) ["user_string", T.pack $ show user_name]
 
-putUserString :: forall qp. QueryParam qp => [qp] -> [Char] -> UserRequest -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+putUserString :: forall qp. QueryParam qp => [qp] -> [Char] -> UserRequest -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 putUserString params user_name user_request = handleError <$> putAt params ["user_string", T.pack $ show user_name] user_request
 
-putUserString' :: [Char] -> UserRequest -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+putUserString' :: [Char] -> UserRequest -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 putUserString' user_name user_request = handleError <$> putAt ([] :: [(Text, Text)]) ["user_string", T.pack $ show user_name] user_request
 
-deleteUserString :: forall qp. QueryParam qp => [qp] -> [Char] -> ApiEff (Either (ApiError ApplicationError) ())
+deleteUserString :: forall qp. QueryParam qp => [qp] -> [Char] -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) ())
 deleteUserString params user_name = handleError <$> deleteAt params ["user_string", T.pack $ show user_name]
 
-deleteUserString' :: [Char] -> ApiEff (Either (ApiError ApplicationError) ())
+deleteUserString' :: [Char] -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) ())
 deleteUserString' user_name = handleError <$> deleteAt ([] :: [(Text, Text)]) ["user_string", T.pack $ show user_name]
 
 -- footer
--- gen/Interop.Api.purs
+++ gen/Interop.Api.purs
@@ -6,94 +6,94 @@ import Prelude                (Unit, show, map, (<$>), (<>))
 import Purescript.Api.Helpers (class QueryParam, ApiError, ApiEff, getAt, handleError, qp, deleteAt, putAt, postAt)
 import Data.Default
 
-getUsers :: forall qp. QueryParam qp => Array qp -> ApiEff (Either (ApiError ApplicationError) UserResponses)
+getUsers :: forall qp. QueryParam qp => Array qp -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponses)
 getUsers params = handleError <$> getAt params ["users"]
 
-getUsers' :: ApiEff (Either (ApiError ApplicationError) UserResponses)
+getUsers' :: ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponses)
 getUsers'  = handleError <$> getAt ([] :: Array Boolean) ["users"]
 
-getUsers_ByUsersIds :: forall qp. QueryParam qp => Array qp -> (Array Int) -> ApiEff (Either (ApiError ApplicationError) UserResponses)
+getUsers_ByUsersIds :: forall qp. QueryParam qp => Array qp -> (Array Int) -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponses)
 getUsers_ByUsersIds params _ByUsersIds = handleError <$> getAt (map qp params <> map qp [ByUsersIds _ByUsersIds]) ["users"]
 
-getUsers_ByUsersIds' :: (Array Int) -> ApiEff (Either (ApiError ApplicationError) UserResponses)
+getUsers_ByUsersIds' :: (Array Int) -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponses)
 getUsers_ByUsersIds' _ByUsersIds = handleError <$> getAt [ByUsersIds _ByUsersIds] ["users"]
 
-getUsers_ByUsersNames :: forall qp. QueryParam qp => Array qp -> (Array String) -> ApiEff (Either (ApiError ApplicationError) UserResponses)
+getUsers_ByUsersNames :: forall qp. QueryParam qp => Array qp -> (Array String) -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponses)
 getUsers_ByUsersNames params _ByUsersNames = handleError <$> getAt (map qp params <> map qp [ByUsersNames _ByUsersNames]) ["users"]
 
-getUsers_ByUsersNames' :: (Array String) -> ApiEff (Either (ApiError ApplicationError) UserResponses)
+getUsers_ByUsersNames' :: (Array String) -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponses)
 getUsers_ByUsersNames' _ByUsersNames = handleError <$> getAt [ByUsersNames _ByUsersNames] ["users"]
 
-getUsers_ByUsersEmails :: forall qp. QueryParam qp => Array qp -> (Array String) -> ApiEff (Either (ApiError ApplicationError) UserResponses)
+getUsers_ByUsersEmails :: forall qp. QueryParam qp => Array qp -> (Array String) -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponses)
 getUsers_ByUsersEmails params _ByUsersEmails = handleError <$> getAt (map qp params <> map qp [ByUsersEmails _ByUsersEmails]) ["users"]
 
-getUsers_ByUsersEmails' :: (Array String) -> ApiEff (Either (ApiError ApplicationError) UserResponses)
+getUsers_ByUsersEmails' :: (Array String) -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponses)
 getUsers_ByUsersEmails' _ByUsersEmails = handleError <$> getAt [ByUsersEmails _ByUsersEmails] ["users"]
 
-getUser_ByUserActive :: forall qp. QueryParam qp => Array qp -> Int -> Boolean -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+getUser_ByUserActive :: forall qp. QueryParam qp => Array qp -> Int -> Boolean -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 getUser_ByUserActive params user_id _ByUserActive = handleError <$> getAt (map qp params <> map qp [ByUserActive _ByUserActive]) ["user", show user_id]
 
-getUser_ByUserActive' :: Int -> Boolean -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+getUser_ByUserActive' :: Int -> Boolean -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 getUser_ByUserActive' user_id _ByUserActive = handleError <$> getAt [ByUserActive _ByUserActive] ["user", show user_id]
 
-postUser :: forall qp. QueryParam qp => Array qp -> UserRequest -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+postUser :: forall qp. QueryParam qp => Array qp -> UserRequest -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 postUser params user_request = handleError <$> postAt params ["user"] user_request
 
-postUser' :: UserRequest -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+postUser' :: UserRequest -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 postUser' user_request = handleError <$> postAt ([] :: Array Boolean) ["user"] user_request
 
-getUser :: forall qp. QueryParam qp => Array qp -> Int -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+getUser :: forall qp. QueryParam qp => Array qp -> Int -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 getUser params user_id = handleError <$> getAt params ["user", show user_id]
 
-getUser' :: Int -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+getUser' :: Int -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 getUser' user_id = handleError <$> getAt ([] :: Array Boolean) ["user", show user_id]
 
-putUser :: forall qp. QueryParam qp => Array qp -> Int -> UserRequest -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+putUser :: forall qp. QueryParam qp => Array qp -> Int -> UserRequest -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 putUser params user_id user_request = handleError <$> putAt params ["user", show user_id] user_request
 
-putUser' :: Int -> UserRequest -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+putUser' :: Int -> UserRequest -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 putUser' user_id user_request = handleError <$> putAt ([] :: Array Boolean) ["user", show user_id] user_request
 
-deleteUser :: forall qp. QueryParam qp => Array qp -> Int -> ApiEff (Either (ApiError ApplicationError) Unit)
+deleteUser :: forall qp. QueryParam qp => Array qp -> Int -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) Unit)
 deleteUser params user_id = handleError <$> deleteAt params ["user", show user_id]
 
-deleteUser' :: Int -> ApiEff (Either (ApiError ApplicationError) Unit)
+deleteUser' :: Int -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) Unit)
 deleteUser' user_id = handleError <$> deleteAt ([] :: Array Boolean) ["user", show user_id]
 
-getUserText :: forall qp. QueryParam qp => Array qp -> String -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+getUserText :: forall qp. QueryParam qp => Array qp -> String -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 getUserText params user_name = handleError <$> getAt params ["user_text", user_name]
 
-getUserText' :: String -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+getUserText' :: String -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 getUserText' user_name = handleError <$> getAt ([] :: Array Boolean) ["user_text", user_name]
 
-putUserText :: forall qp. QueryParam qp => Array qp -> String -> UserRequest -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+putUserText :: forall qp. QueryParam qp => Array qp -> String -> UserRequest -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 putUserText params user_name user_request = handleError <$> putAt params ["user_text", user_name] user_request
 
-putUserText' :: String -> UserRequest -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+putUserText' :: String -> UserRequest -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 putUserText' user_name user_request = handleError <$> putAt ([] :: Array Boolean) ["user_text", user_name] user_request
 
-deleteUserText :: forall qp. QueryParam qp => Array qp -> String -> ApiEff (Either (ApiError ApplicationError) Unit)
+deleteUserText :: forall qp. QueryParam qp => Array qp -> String -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) Unit)
 deleteUserText params user_name = handleError <$> deleteAt params ["user_text", user_name]
 
-deleteUserText' :: String -> ApiEff (Either (ApiError ApplicationError) Unit)
+deleteUserText' :: String -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) Unit)
 deleteUserText' user_name = handleError <$> deleteAt ([] :: Array Boolean) ["user_text", user_name]
 
-getUserString :: forall qp. QueryParam qp => Array qp -> String -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+getUserString :: forall qp. QueryParam qp => Array qp -> String -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 getUserString params user_name = handleError <$> getAt params ["user_string", user_name]
 
-getUserString' :: String -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+getUserString' :: String -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 getUserString' user_name = handleError <$> getAt ([] :: Array Boolean) ["user_string", user_name]
 
-putUserString :: forall qp. QueryParam qp => Array qp -> String -> UserRequest -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+putUserString :: forall qp. QueryParam qp => Array qp -> String -> UserRequest -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 putUserString params user_name user_request = handleError <$> putAt params ["user_string", user_name] user_request
 
-putUserString' :: String -> UserRequest -> ApiEff (Either (ApiError ApplicationError) UserResponse)
+putUserString' :: String -> UserRequest -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) UserResponse)
 putUserString' user_name user_request = handleError <$> putAt ([] :: Array Boolean) ["user_string", user_name] user_request
 
-deleteUserString :: forall qp. QueryParam qp => Array qp -> String -> ApiEff (Either (ApiError ApplicationError) Unit)
+deleteUserString :: forall qp. QueryParam qp => Array qp -> String -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) Unit)
 deleteUserString params user_name = handleError <$> deleteAt params ["user_string", user_name]
 
-deleteUserString' :: String -> ApiEff (Either (ApiError ApplicationError) Unit)
+deleteUserString' :: String -> ApiEff SpecificApiOptions (Either (ApiError ApplicationError) Unit)
 deleteUserString' user_name = handleError <$> deleteAt ([] :: Array Boolean) ["user_string", user_name]
 
 -- footer
